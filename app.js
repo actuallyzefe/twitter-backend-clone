e24788175715075c80bfb3de2052825d82179f3c
Efe Karakanlı <102619516+actuallyzefe@users.noreply.github.com>
2023-02-08 22:23:39 +0300

    init

--- app.js
+++ app.js
@@ -1,10 +1,7 @@
 const express = require('express');
 const app = express();
-const cors = require('cors');
+
 const authRoutes = require('./routes/authRoutes');
-const passport = require('passport');
-const PassportLocal = require('passport-local');
-const router = require('./routes/authRoutes');
 
 // app.set('view engine', 'ejs');
 app.use(express.json());
