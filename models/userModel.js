230891148a76f5b16d3cc46c97bb47f99f63816e
Efe Karakanlı <102619516+actuallyzefe@users.noreply.github.com>
2023-03-02 12:56:11 +0300

    enum fixed

--- models/userModel.js
+++ models/userModel.js
@@ -24,7 +24,7 @@ const userSchema = new mongoose.Schema(
 
     role: {
       type: String,
-      enum: ['user', 'guide', 'lead-guide', 'admin'],
+      enum: ['user', 'admin'],
       default: 'user',
     },
 
