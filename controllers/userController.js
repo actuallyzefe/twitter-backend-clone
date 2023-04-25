390dccc55c9b2490b3253b0f1979ea93e27dd82d
Efe Karakanlı <102619516+actuallyzefe@users.noreply.github.com>
2023-04-25 23:04:43 +0300

    fix: interesting bug fixed nothing changed ? :D

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,16 +4,6 @@ const catchAsync = require('../utils/catchAsync');
 const multer = require('multer');
 const sharp = require('sharp');
 
-// const multerStorage = multer.diskStorage({
-//   destination: (req, file, cb) => {
-//     cb(null, 'public/images/users');
-//   },
-//   filename: (req, file, cb) => {
-//     const ext = file.mimetype.split('/')[1];
-//     cb(null, `user-${req.user.id}-${Date.now()}.${ext}`);
-//   },
-// });
-
 const multerStorage = multer.memoryStorage();
 
 const multerFilter = (req, file, cb) => {
